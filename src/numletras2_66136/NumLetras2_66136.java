8bf14a75db3e7f1488c007d64b28ba7c80a04603
salva <salva@LAPTOP-B2KFIIUB>
2021-02-24 22:49:19 -0600

    Tarea de numeros a letras 66136

---
+++ src/numletras2_66136/NumLetras2_66136.java
@@ -0,0 +1,335 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numletras2_66136;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author salva
+ */
+public class NumLetras2_66136 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   Scanner entrada = new Scanner(System.in);
+        
+        int nu=99;
+        System.out.println("Ingrese un numero de 0 al 99: ");
+        nu=entrada.nextInt();
+          switch (nu){
+              case 0:
+                  System.out.println("Cero");
+                  break;
+                  case 1:
+                  System.out.println("uno");
+                  break;
+                  case 2:
+                  System.out.println("dos");
+                  break;
+                  case 3:
+                  System.out.println("tres");
+                  break;
+                  case 4:
+                  System.out.println("cuatro");
+                  break;
+                  case 5:
+                  System.out.println("cinco");
+                  break;
+                  case 6:
+                  System.out.println("seis");
+                  break;
+                  case 7:
+                  System.out.println("siete");
+                  break;
+                  case 8:
+                  System.out.println("ocho");
+                  break;
+                  case 9:
+                  System.out.println("nueve");
+                  break;
+                  case 10:
+                  System.out.println("Diez");
+                  break;
+                  case 11:
+                  System.out.println("once");
+                  break;
+                  case 12:
+                  System.out.println("doce");
+                  break;
+                  case 13:
+                  System.out.println("trece");
+                  break;
+                  case 14:
+                  System.out.println("catorce");
+                  break;
+                  case 15:
+                  System.out.println("quince");
+                  break;
+                  case 16:
+                  System.out.println("dieciseis");
+                  break;
+                  case 17:
+                  System.out.println("diecisiete");
+                  break;
+                  case 18:
+                  System.out.println("dieciocho");
+                  break;
+                  case 19:
+                  System.out.println("diecinueve");
+                  break;
+                  case 20:
+                  System.out.println("veinte");
+                  break;
+                   case 21:
+                  System.out.println("veintiuno");
+                  break;
+                  case 22:
+                  System.out.println("veintidos");
+                  break;
+                  case 23:
+                  System.out.println("veintitres");
+                  break;
+                  case 24:
+                  System.out.println("veinticuatro");
+                  break;
+                  case 25:
+                  System.out.println("veinticinco");
+                  break;
+                  case 26:
+                  System.out.println("veintiseis");
+                  break;
+                  case 27:
+                  System.out.println("veintisiete");
+                  break;
+                  case 28:
+                  System.out.println("veintiocho");
+                  break;
+                  case 29:
+                  System.out.println("veintinueve");
+                  break;
+                  case 30:
+                  System.out.println("treinta");
+                  break;
+                   case 31:
+                  System.out.println("treinta y uno");
+                  break;
+                  case 32:
+                  System.out.println("treinta y dos");
+                  break;
+                  case 33:
+                  System.out.println("treinta y tres");
+                  break;
+                  case 34:
+                  System.out.println("treinta y cuatro");
+                  break;
+                  case 35:
+                  System.out.println("treinta y cinco");
+                  break;
+                  case 36:
+                  System.out.println("treinta y seis");
+                  break;
+                  case 37:
+                  System.out.println("treinta y siete");
+                  break;
+                  case 38:
+                  System.out.println("treinta y ocho");
+                  break;
+                  case 39:
+                  System.out.println("treinta y nueve");
+                  break;
+                  case 40:
+                  System.out.println("Cuarenta");
+                  break;
+                   case 41:
+                  System.out.println("Cuarenta y uno");
+                  break;
+                  case 42:
+                  System.out.println("Cuarenta y dos");
+                  break;
+                  case 43:
+                  System.out.println("Cuarenta y tres");
+                  break;
+                  case 44:
+                  System.out.println("Cuarenta y cuatro");
+                  break;
+                  case 45:
+                  System.out.println("Cuarenta y cinco");
+                  break;
+                  case 46:
+                  System.out.println("Cuarenta y seis");
+                  break;
+                  case 47:
+                  System.out.println("Cuarenta y siete");
+                  break;
+                  case 48:
+                  System.out.println("Cuarenta y ocho");
+                  break;
+                  case 49:
+                  System.out.println("Cuarenta y nueve");
+                  break;
+                  case 50:
+                  System.out.println("cincuenta");
+                  break;
+                   case 51:
+                  System.out.println("cincuenta y uno");
+                  break;
+                  case 52:
+                  System.out.println("cincuenta y dos");
+                  break;
+                  case 53:
+                  System.out.println("cincuenta y tres");
+                  break;
+                  case 54:
+                  System.out.println("cincuenta y cuatro");
+                  break;
+                  case 55:
+                  System.out.println("cincuenta y cinco");
+                  break;
+                  case 56:
+                  System.out.println("cincuenta y seis");
+                  break;
+                  case 57:
+                  System.out.println("cincuenta y siete");
+                  break;
+                  case 58:
+                  System.out.println("cincuenta y ocho");
+                  break;
+                  case 59:
+                  System.out.println("cincuenta y nueve");
+                  break;
+                  case 60:
+                  System.out.println("sesenta");
+                  break;
+                   case 61:
+                  System.out.println("sesenta y uno");
+                  break;
+                  case 62:
+                  System.out.println("sesenta y dos");
+                  break;
+                  case 63:
+                  System.out.println("sesenta y tres");
+                  break;
+                  case 64:
+                  System.out.println("sesenta y cuatro");
+                  break;
+                  case 65:
+                  System.out.println("sesenta y cinco");
+                  break;
+                  case 66:
+                  System.out.println("sesenta y seis");
+                  break;
+                  case 67:
+                  System.out.println("sesenta y siete");
+                  break;
+                  case 68:
+                  System.out.println("sesenta y ocho");
+                  break;
+                  case 69:
+                  System.out.println("sesenta y nueve");
+                  break;
+                  case 70:
+                  System.out.println("setenta");
+                  break;
+                   case 71:
+                  System.out.println("setenta y uno");
+                  break;
+                  case 72:
+                  System.out.println("setenta y dos");
+                  break;
+                  case 73:
+                  System.out.println("setenta y tres");
+                  break;
+                  case 74:
+                  System.out.println("setenta y cuatro");
+                  break;
+                  case 75:
+                  System.out.println("setenta y cinco");
+                  break;
+                  case 76:
+                  System.out.println("setenta y seis");
+                  break;
+                  case 77:
+                  System.out.println("setenta y siete");
+                  break;
+                  case 78:
+                  System.out.println("setenta y ocho");
+                  break;
+                  case 79:
+                  System.out.println("setenta y nueve");
+                  break;
+                  case 80:
+                  System.out.println("ochenta");
+                  break;
+                   case 81:
+                  System.out.println("ochenta y uno");
+                  break;
+                  case 82:
+                  System.out.println("ochenta y dos");
+                  break;
+                  case 83:
+                  System.out.println("ochenta y tres");
+                  break;
+                  case 84:
+                  System.out.println("ochenta y cuatro");
+                  break;
+                  case 85:
+                  System.out.println("ochenta y cinco");
+                  break;
+                  case 86:
+                  System.out.println("ochenta y seis");
+                  break;
+                  case 87:
+                  System.out.println("ochenta y siete");
+                  break;
+                  case 88:
+                  System.out.println("ochenta y ocho");
+                  break;
+                  case 89:
+                  System.out.println("ochenta y nueve");
+                  break;
+                  case 90:
+                  System.out.println("noventa");
+                  break;
+                   case 91:
+                  System.out.println("noventa y uno");
+                  break;
+                  case 92:
+                  System.out.println("noventa y dos");
+                  break;
+                  case 93:
+                  System.out.println("noventa y tres");
+                  break;
+                  case 94:
+                  System.out.println("noventa y cuatro");
+                  break;
+                  case 95:
+                  System.out.println("noventa y cinco");
+                  break;
+                  case 96:
+                  System.out.println("noventa y seis");
+                  break;
+                  case 97:
+                  System.out.println("noventa y siete");
+                  break;
+                  case 98:
+                  System.out.println("noventa y ocho");
+                  break;
+                  case 99:
+                  System.out.println("noventa y nueve");
+                  break;
+                  default:
+                     System.out.println("Indeterminados");
+          
+          
+          }
+                
+    } }
+    
+
